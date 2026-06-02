303947f676ef2ec98539658980defdd90a187a6e
fino <fintanjnr@gmail.com>
2026-06-02 14:50:19 +0100

    Added the Receipt folder and receipt entity file

---
+++ app/api/src/main/java/com/scanit/receipt/Receipt.java
@@ -0,0 +1,37 @@
+package com.scanit.receipt;
+
+import com.scanit.user.model.User2;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "receipts")
+@Data
+@NoArgsConstructor
+public class Receipt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String vendorName;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal transactionAmount;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal totalAmount;
+
+    @Column(nullable = false)
+    private LocalDate transactionDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User2 user;
+}
