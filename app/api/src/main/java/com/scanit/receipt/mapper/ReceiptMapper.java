e3449c5bf0fff192dd974ac5bdf24d24729ab819
fino <fintanjnr@gmail.com>
2026-06-02 20:37:12 +0100

    Added receipt mapper folder & file

---
+++ app/api/src/main/java/com/scanit/receipt/mapper/ReceiptMapper.java
@@ -0,0 +1,16 @@
+package com.scanit.receipt.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.scanit.receipt.model.Receipt;
+import com.scanit.receipt.dto.ReceiptDTO;
+
+@Mapper(componentModel = "spring")
+public interface ReceiptMapper {
+    @Mapping(source = "user.id", target = "userId")
+    ReceiptDTO toDTO(Receipt receipt);
+
+    @Mapping(target = "user", ignore = true)
+    Receipt toEntity(ReceiptDTO dto);
+}
