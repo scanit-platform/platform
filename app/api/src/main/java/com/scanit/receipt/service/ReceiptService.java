4e9b887fcee991d0c9a8a32b3992ce0458740f0d
fino <fintanjnr@gmail.com>
2026-06-02 20:17:02 +0100

    Added service & service implementation files

---
+++ app/api/src/main/java/com/scanit/receipt/service/ReceiptService.java
@@ -0,0 +1,18 @@
+package com.scanit.receipt.service;
+
+import com.scanit.receipt.model.Receipt;
+import com.scanit.receipt.dto.ReceiptDTO;
+import java.util.Optional;
+import java.util.List;
+import java.time.LocalDate;
+
+public interface ReceiptService {
+    ReceiptDTO save(ReceiptDTO dto);
+    Optional<Receipt> deleteById(Long id);
+    List<Receipt> findAll();
+    Optional<Receipt> findById(Long id);
+    void deleteByReceiptId(Long id);
+
+    List<Receipt> findByVendorName(String vendorName);
+    List<Receipt> findByTransactionDate(LocalDate transactionDate);
+}
