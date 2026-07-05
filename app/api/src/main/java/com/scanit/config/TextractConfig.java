bc401d4c3f230d73f4bd5cfe869498bd436d0aee
Syed Ali Raza <isyedaliraza@gmail.com>
2026-07-05 20:58:50 +0100

    fix: AWS textract auth error

--- app/api/src/main/java/com/scanit/config/TextractConfig.java
+++ app/api/src/main/java/com/scanit/config/TextractConfig.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.textract.TextractClient;
 
@@ -13,11 +14,17 @@ public class TextractConfig {
     @Value("${spring.cloud.aws.region.static}")
     private String region;
     
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+    
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    
     @Bean
     public TextractClient textractClient() {
         return TextractClient.builder()
         .region(Region.of(region))
-        .credentialsProvider(DefaultCredentialsProvider.builder().build())
+        .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
         .build();
     }
 }
